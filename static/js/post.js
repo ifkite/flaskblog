678c30629f134499b55a0cd13eaae626cdbb27fe
ifkite <if.kite@gmail.com>
2015-03-03 01:46:21 +0800

    delete static files that not used

--- static/js/post.js
+++
@@ -1,9 +0,0 @@
-//$(document).ready(function(){
-    $('#mycomment').click(function(){
-        $.post('/_comment',$('#commentform').serialize(),
-            function(data){
-                $("#commentlist").append(data.comment_html);
-            }
-        );
-    });
-//});
